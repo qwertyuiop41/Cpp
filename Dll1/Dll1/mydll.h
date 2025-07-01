263fbe2764e37473ada2fc9e8e215308a6209032
qwertyuiop41 <3111205688@qq.com>
2025-07-01 16:55:23 +0800

    delete dll

--- Dll1/Dll1/mydll.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class mydll
-{
-	int sum(int a, int b);
-	int div(int a, int b);
-	void fun1(char a, char c);
-};
-
--- mydll/mydll/mydll.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <windows.h>
-class __declspec(dllexport) mydll {
-public:
-	static int sum(int a, int b);
-	static int div(int a, int b);
-	static void fun1(char a, char c);
-};
-
