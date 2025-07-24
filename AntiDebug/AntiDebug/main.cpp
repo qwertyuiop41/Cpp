c6c437d1468fff54aa98f3aa803a1501dbfacd5d
qwertyuiop41 <3111205688@qq.com>
2025-07-24 15:56:35 +0800

    简单的反调试尝试

---
+++ AntiDebug/AntiDebug/main.cpp
@@ -0,0 +1,66 @@
+#include <Windows.h>
+#include <iostream>
+
+
+//int main() {
+//
+//	__try {
+//		char* str = NULL;
+//		str[0] = 'a';
+//	}
+//	__except(1){
+//		printf("Exception!\n");
+//	}
+//
+//	printf("hello world\n");
+//	system("pause");
+//	return 0;
+//}
+
+EXCEPTION_DISPOSITION  myExceptHandler(
+	struct _EXCEPTION_RECORD* ExceptionRecord,
+	PVOID EstablisherFrame,
+	PCONTEXT pcontext, //寄存器环境
+	PVOID DispatcherContext
+)
+{
+	MessageBoxA(0, "异常处理代码执行了！", "提示", MB_OK);
+
+	DWORD isDebug = 0;
+	__asm {
+		mov eax,fs:[0x18] //teb
+		mov eax,[eax+0x30] //peb
+		movzx eax, byte ptr[eax + 0x2] //peb->BeingDebugged
+		mov isDebug, eax //将结果存储到isDebug中
+	}
+	printf("%d\n", isDebug);
+	if (isDebug)
+	{
+		MessageBoxA(0, "正在被调试", "提示", MB_OK);
+		exit(0);
+	}
+
+	pcontext->Eip = pcontext->Eip + 3;
+
+	return ExceptionContinueExecution;
+}
+
+
+
+int main() {
+
+	DWORD exceptionHandler = (DWORD)myExceptHandler;
+	__asm {
+		push exceptionHandler
+		mov eax, fs:[0]
+		push eax
+		mov fs:[0],esp
+	}
+
+	char* str = NULL;
+	str[0] = 'a';
+
+	printf("hello world\n");
+	system("pause");
+	return 0;
+}
